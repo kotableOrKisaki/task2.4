86b211e53f072ceb8ad74d0fe07b4308a04eaa65
kotableOrKisaki <super_misha05@mail.ru>
2022-08-27 23:29:18 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(0,101);
+            int sumEnumeration = 0;
+            int enumeration = 0;
+
+            Console.WriteLine("Случайное число - " + number);
+            Console.WriteLine("Все положительные числа <= {0}, кратные 3 или 5:", number);
+            while (enumeration <= number)
+            {
+                enumeration++;
+                if (enumeration%3 == 0 || enumeration%5 == 0)
+                {
+                    sumEnumeration += enumeration;
+                    Console.WriteLine(enumeration);
+                }
+            }
+            Console.Write("Cумма всех положительных чисел <= number, кратные 3 или 5:\t" + sumEnumeration);
+        }
+    }
+}
